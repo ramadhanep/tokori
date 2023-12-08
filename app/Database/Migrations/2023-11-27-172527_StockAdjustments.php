a0526a0c0def5cffe8bc205fed1fd36b0be93bfa
Romadhan Prasetyo <romadhanedy@gmail.com>
2023-12-08 16:38:12 +0700

    DB(migration): Update relation stock adjustments

--- app/Database/Migrations/2023-11-27-172527_StockAdjustments.php
+++ app/Database/Migrations/2023-11-27-172527_StockAdjustments.php
@@ -9,12 +9,13 @@ class StockAdjustments extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id'         => ['type' => 'CHAR', 'constraint' => 10],
-            'date'       => ['type' => 'TIMESTAMP'],
-            'note'       => ['type' => 'TEXT', 'null' => true],
-            'created_at' => ['type' => 'TIMESTAMP', 'null' => true],
-            'updated_at' => ['type' => 'TIMESTAMP', 'null' => true],
-            'deleted_at' => ['type' => 'TIMESTAMP', 'null' => true],
+            'id'            => ['type' => 'CHAR', 'constraint' => 10],
+            'supplier_id'   => ['type' => 'CHAR', 'constraint' => 10],
+            'date'          => ['type' => 'TIMESTAMP'],
+            'note'          => ['type' => 'TEXT', 'null' => true],
+            'created_at'    => ['type' => 'TIMESTAMP', 'null' => true],
+            'updated_at'    => ['type' => 'TIMESTAMP', 'null' => true],
+            'deleted_at'    => ['type' => 'TIMESTAMP', 'null' => true],
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('stock_adjustments', true);
