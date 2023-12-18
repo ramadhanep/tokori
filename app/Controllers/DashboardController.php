4f573e8de8cac46a9a6cb52eeb8034a5df36b49c
Romadhan Prasetyo <romadhanedy@gmail.com>
2023-12-19 01:52:04 +0700

    Dashboard(UI): Initial

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -10,26 +10,9 @@ class DashboardController extends BaseController
     {
         $profile = new User();
         $profile = $profile->find(session()->get('SES_AUTH_USER_ID'));
-
-        $hour = date('H');
-        $greeting = $this->greeting($hour);
         
         $pass['profile'] = $profile;
-        $pass['greeting'] = $greeting;
 
         return view("pages/home", $pass);
-    }
-
-    private function greeting($hour)
-    {
-        if ($hour >= 0 && $hour < 12) {
-            return 'Pagi';
-        } elseif ($hour >= 12 && $hour < 15) {
-            return 'Siang';
-        } elseif ($hour >= 15 && $hour < 18) {
-            return 'Sore';
-        } else {
-            return 'Malam';
-        }
     }
 }
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -10,11 +10,11 @@
         <div class="card-body">
           <div class="card-title d-flex align-items-start justify-content-between">
             <div class="avatar flex-shrink-0">
-              <img src="/img/icons/unicons/wallet.png" alt="transaksi" class="rounded">
+              <img src="/img/icons/unicons/cc-warning.png" alt="transaksi" class="rounded">
             </div>
           </div>
-          <span class="fw-semibold d-block mt-3 mb-2">Jumlah Transaksi</span>
-          <h3 class="card-title mb-3"><?= 1000 ?></h3>
+          <span class="fw-semibold d-block mt-3 mb-2">Total Transaksi Bulan ini</span>
+          <h3 class="card-title mb-3"><?= 0 ?></h3>
         </div>
       </div>
     </div>
@@ -23,31 +23,37 @@
         <div class="card-body">
           <div class="card-title d-flex align-items-start justify-content-between">
             <div class="avatar flex-shrink-0">
-              <img src="/img/icons/unicons/paypal.png" alt="transaksi" class="rounded">
+              <img src="/img/icons/unicons/wallet-info.png" alt="transaksi" class="rounded">
             </div>
           </div>
-          <span class="fw-semibold d-block mt-3 mb-2">Valuasi Transaksi Selesai</span>
+          <span class="fw-semibold d-block mt-3 mb-2">Pendapatan Bulan ini</span>
+          <h3 class="card-title mb-3"><?= rupiahFormatDashboard(100000) ?></h3>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title d-flex align-items-start justify-content-between">
+            <div class="avatar flex-shrink-0">
+              <img src="/img/icons/unicons/chart-success.png" alt="transaksi" class="rounded">
+            </div>
+          </div>
+          <span class="fw-semibold d-block mt-3 mb-2">Total Produk</span>
           <h3 class="card-title mb-3"><?= rupiahFormatDashboard(100000) ?></h3>
         </div>
       </div>
     </div>
   </div>
-  <div class="row mt-4">
-    <div class="col-lg-8 mb-4 order-0">
+  <div class="mt-4 row">
+    <div class="col-md-12 col-lg-9 mb-4">
       <div class="card">
-        <div class="d-flex align-items-end row">
-          <div class="col-sm-9">
-            <div class="card-body">
-              <h5 class="card-title text-primary"><?= $greeting ?> <?= $profile["name"] ?> 👋</h5>
-              <p class="mb-4">Selamat bekerja dengan penuh semangat.</p>
-              <a href="<?= site_url('profile') ?>" class="btn btn-sm btn-label-primary">Lihat Profile</a>
-            </div>
-          </div>
-          <div class="col-sm-3 text-center text-sm-left">
-            <div class="card-body pb-0 px-0 px-md-4">
-              <img src="/img/illustrations/man-with-laptop-light.png" height="140" alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.png">
-            </div>
-          </div>
+        <div class="card-header">
+          <h5 class="card-title mb-0">Total Pendapatan</h5>
+          <small class="card-subtitle">Data Tahun 2023</small>
+        </div>
+        <div class="card-body">
+          <div id="totalIncomeChart"></div>
         </div>
       </div>
     </div>
@@ -60,6 +66,104 @@
 <?= $this->endSection() ?>
 
 <?= $this->section("scripts") ?>
+<script src="/vendor/libs/apex-charts/apexcharts.js"></script>
 <script>
+"use strict";
+
+(function () {
+  // Define colors
+  const cardColor = config.colors.cardColor;
+  const headingColor = config.colors.headingColor;
+  const textMuted = config.colors.textMuted;
+  const borderColor = config.colors.borderColor;
+
+  // Define gradient colors
+  const gradientShade = "";
+  const gradientShadeIntensity = 0.8;
+  const gradientOpacityFrom = 0.7;
+  const gradientOpacityTo = 0.25;
+  const gradientStops = [0, 95, 100];
+
+  // Total Income Chart
+  const totalIncomeChartOptions = {
+    chart: {
+      height: 250,
+      type: "area",
+      toolbar: false,
+      dropShadow: {
+        enabled: true,
+        top: 14,
+        left: 2,
+        blur: 3,
+        color: config.colors.primary,
+        opacity: 0.15,
+      },
+    },
+    series: [
+      {
+        data: [
+          3350000, 3350000, 4800000, 4800000, 2950000, 2950000, 1800000, 1800000, 3750000, 3750000, 5700000, 5700000,
+        ],
+      },
+    ],
+    dataLabels: { enabled: false },
+    stroke: { width: 3, curve: "straight" },
+    colors: [config.colors.primary],
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: gradientShade,
+        shadeIntensity: gradientShadeIntensity,
+        opacityFrom: gradientOpacityFrom,
+        opacityTo: gradientOpacityTo,
+        stops: gradientStops,
+      },
+    },
+    grid: {
+      show: true,
+      borderColor: borderColor,
+      padding: { top: -15, bottom: -10, left: 0, right: 0 },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "Mei",
+        "Jun",
+        "Jul",
+        "Agt",
+        "Sep",
+        "Okt",
+        "Nov",
+        "Des",
+      ],
+      labels: { offsetX: 0, style: { colors: textMuted, fontSize: "13px" } },
+      axisBorder: { show: false },
+      axisTicks: { show: false },
+      lines: { show: false },
+    },
+    yaxis: {
+      labels: {
+        offsetX: -15,
+        formatter: function (val) {
+          return "Rp" + parseInt(val / 1e6) + "jt";
+        },
+        style: { fontSize: "13px", colors: textMuted },
+      },
+      min: 1e6,
+      max: 6e6,
+      tickAmount: 5,
+    },
+  };
+
+  const totalIncomeChartElement = document.querySelector("#totalIncomeChart");
+  if (totalIncomeChartElement) {
+    new ApexCharts(totalIncomeChartElement, totalIncomeChartOptions).render();
+  }
+
+})();
+
 </script>
 <?= $this->endSection() ?>
